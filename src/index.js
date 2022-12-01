c59ce98022162f2dcdcc37b47202ee48485d04be
Black-Male <Gkaranja20@gmail.com>
2022-12-01 13:52:48 +0300

    work finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e) => {
+    e.preventDefault()
+    handleToDo(e.target.querySelector('#new-task-description').value)
+    form.reset()
+  })
+
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  let bttn = document.createElement('button')
+  bttn.addEventListener('click', DeleteTask)
+  bttn.textContent = '  x  '
+  p.textContent = `${todo}`
+  p.appendChild(bttn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function DeleteTask(e){
+  e.target.parentNode.remove()
+}
